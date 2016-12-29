9417d8ec26d4b9edf6aade80aae782e3850aebea
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-12-29 16:11:38 +0200

    added cards Mine and Witch + according strategies; added proper command line arguments incl dynamic strategy loading

--- regno.py
+++ regno.py
@@ -2,19 +2,22 @@
 
 from __future__ import unicode_literals
 
+import argparse
 import json
 import logging
 import random
 import sys
 
 from collections import Counter
+from importlib import import_module
 
 LOGGER = logging.getLogger(__name__)
 
 class Game(object):
     def __init__(self, supply, strategies):
         self.supply = {card: card.supply for card in supply}
-        self.players = [Player(self, strategy) for strategy in strategies]
+        self.players = [Player('Player #{}'.format(i + 1), self, strategy)
+                        for i, strategy in enumerate(strategies)]
         random.shuffle(self.players)
 
         self.current_round = 0
@@ -70,6 +73,8 @@ class Game(object):
                 LOGGER.info('Player #%d has won!', i + 1)
 
     def play_round(self, player):
+        LOGGER.info('hand: [%s]', ', '.join(card(player, self).name for card in player.hand))
+
         player.actions = 1
         player.buys = 1
         player.spent_money = 0
@@ -122,7 +127,8 @@ class Game(object):
         LOGGER.info('player is done with the turn – next one')
 
 class Player(object):
-    def __init__(self, game, strategy):
+    def __init__(self, name, game, strategy):
+        self.name = name
         self.game = game
         self.strategy = strategy
         self.deck = [Copper] * 7 + [Estate] * 3
@@ -136,6 +142,9 @@ class Player(object):
             self.draw_hand()
 
         self.spent_money = 0
+
+    def __str__(self):
+        return self.name
 
     @property
     def victory_points(self):
@@ -165,6 +174,7 @@ class Player(object):
         try:
             return self.deck.pop(0)
         except IndexError:
+            LOGGER.info('empty deck – have to shuffle discard pile')
             self.deck = self.discard_pile
             self.discard_pile = []
             random.shuffle(self.deck)
@@ -173,12 +183,18 @@ class Player(object):
     def draw_hand(self):
         card = self.draw()
         if card:
+            LOGGER.info('drew card %s to hand', card.__name__)
             self.hand.append(card)
+        else:
+            LOGGER.warning('unable to draw card to hand')
 
 class Card(object):
     def __init__(self, player, game):
         self.player = player
         self.game = game
+
+    def __str__(self):
+        return self.name
 
     def play(self):
         self.player.in_play.append(self)
@@ -186,12 +202,16 @@ class Card(object):
         self.player.buys += self.buys
         for _ in range(self.cards):
             self.player.draw_hand()
-        # self.player.hand.extend(self.player.draw() for _ in range(self.cards))
+
+    def gain(self):
+        self.game.supply[type(self)] -= 1
+        self.player.discard_pile.append(type(self))
+        LOGGER.info('player %s gained card %s', self.player, self)
 
     def buy(self):
         self.player.spent_money += self.cost
-        self.game.supply[type(self)] -= 1
-        self.player.discard_pile.append(type(self))
+        LOGGER.info('player %s bought card %s', self.player, self)
+        self.gain()
 
     @property
     def name(self): return type(self).__name__
@@ -246,6 +266,12 @@ class Province(Card):
     supply = 12
     victory_points = 6
 
+class Curse(Card):
+    types = frozenset(['curse'])
+    _cost = 0
+    supply = 30
+    victory_points = -1
+
 class Chancellor(Card):
     types = frozenset(['action'])
     _cost = 3
@@ -285,6 +311,47 @@ class Market(Card):
     cards = 1
     money = 1
 
+class Mine(Card):
+    def __init__(self, player, game, to_trash=None, to_gain=None):
+        super().__init__(player, game)
+        self.to_trash = to_trash
+        self.to_gain = to_gain
+        if isinstance(self.to_gain, type):
+            self.to_gain = {self.to_gain}
+
+    types = frozenset(['action'])
+    _cost = 5
+    supply = 10
+
+    def play(self):
+        super().play()
+
+        if not self.to_trash:
+            # TODO test if no treasure in hand
+            return
+
+        self.player.hand.remove(self.to_trash)
+        self.game.trash.append(self.to_trash)
+
+        LOGGER.info('trashed %s', self.to_trash.__name__)
+
+        money = self.to_trash(self.player, self.game).cost + 3
+
+        gainable = [x[0] for x in self.game.supply.items()
+                    if x[1] > 0 and 'treasure' in x[0].types
+                        and x[0](self.player, self.game).cost <= money]
+
+        if self.to_gain:
+            gainable = [card for card in gainable if card in self.to_gain]
+
+        gainable = sorted(gainable, key=lambda x: -x(self.player, self.game).cost)
+
+        if gainable:
+            gainable[0](self.player, self.game).gain()
+            gained = self.player.discard_pile.pop()
+            self.player.hand.append(gained)
+            LOGGER.info('gained %s', gained.__name__)
+
 class Smithy(Card):
     types = frozenset(['action'])
     _cost = 4
@@ -297,6 +364,24 @@ class Village(Card):
     supply = 10
     actions = 2
     cards = 1
+
+class Witch(Card):
+    types = frozenset(['action', 'attack'])
+    _cost = 5
+    supply = 10
+    cards = 2
+
+    def play(self):
+        super().play()
+
+        for player in self.game.players:
+            if not self.game.supply.get(Curse):
+                break
+
+            if player is self.player:
+                continue
+
+            Curse(player, self.game).gain()
 
 class Woodcutter(Card):
     types = frozenset(['action'])
@@ -361,7 +446,7 @@ class BigMoney(Strategy):
         buyable = sorted(buyable, key=lambda x: -x[0](player, game).cost)
         return buyable[0][0](player, game) if buyable else None
 
-class BigMoneySmithy(BigMoney):
+class BigMoneySmithy(BigMoney, Smarter):
     def buy(self, player, game):
         LOGGER.info('player has %d buy(s) and %d money', player.buys, player.money)
 
@@ -370,21 +455,98 @@ class BigMoneySmithy(BigMoney):
 
         return super().buy(player, game)
 
-if __name__ == '__main__':
-    LOGGER.addHandler(logging.StreamHandler(sys.stdout))
-    LOGGER.setLevel(logging.WARNING)
+class BigMoneyFestival(BigMoneySmithy):
+    def buy(self, player, game):
+        LOGGER.info('player has %d buy(s) and %d money', player.buys, player.money)
 
-    strategies = (BigMoney, BigMoney, BigMoneySmithy, BigMoneySmithy)
+        if player.money == 5 and game.supply.get(Festival) and player.counter[Festival] < 2:
+            return Festival(player, game)
+
+        return super().buy(player, game)
+
+class BigMoneyMiner(BigMoney):
+    def action(self, player, game):
+        LOGGER.info('player has %d action(s)', player.actions)
+
+        if Mine in player.hand and (Copper in player.hand or Silver in player.hand):
+            if Silver in player.hand:
+                return Mine(player, game, Silver)
+            else:
+                return Mine(player, game, Copper)
+
+        return super().action(player, game)
+
+    def buy(self, player, game):
+        LOGGER.info('player has %d buy(s) and %d money', player.buys, player.money)
+
+        if 5 <= player.money <= 7 and game.supply.get(Mine) and player.counter[Mine] < 3:
+            return Mine(player, game)
+
+        return super().buy(player, game)
+
+class BigMoneyWitch(BigMoney, Smarter):
+    def buy(self, player, game):
+        LOGGER.info('player has %d buy(s) and %d money', player.buys, player.money)
+
+        if player.money == 5 and game.supply.get(Witch) and player.counter[Witch] < 3:
+            return Witch(player, game)
+
+        return super().buy(player, game)
+
+def class_from_path(path):
+    parts = path.split('.')
+
+    try:
+        if len(parts) == 1:
+            return globals().get(path) or import_module(path)
+
+        else:
+            obj = import_module(parts[0])
+            for part in parts[1:]:
+                if not obj:
+                    break
+                obj = getattr(obj, part, None)
+            return obj
+
+    except ImportError:
+        return None
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('strategies', nargs='*',
+                        help='strategies')
+    parser.add_argument('-g', '--games', type=int, default=10,
+                        help='number of games')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='log verbosity (repeat to increase)')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    LOGGER.addHandler(logging.StreamHandler(sys.stdout))
+    LOGGER.setLevel(logging.DEBUG if args.verbose >= 2
+                    else logging.INFO if args.verbose == 1
+                    else logging.WARNING)
+
+    LOGGER.info(args.strategies)
+
+    # strategies = (BigMoney, BigMoneySmithy, BigMoneyMiner, BigMoneyFestival)
+    strategies = [class_from_path(strategy) for strategy in args.strategies]
+    LOGGER.info(strategies)
     stats = {strategy.__name__: 0 for strategy in strategies}
     summaries = []
 
-    for i in range(int(sys.argv[1] if len(sys.argv) > 1 else 100)):
+    for i in range(args.games):
         LOGGER.info('\n\n#######################################################')
         LOGGER.info('##################### Game #%05d #####################', i + 1)
         LOGGER.info('#######################################################')
-        game = Game([Copper, Silver, Gold, Estate, Duchy, Province,
+        game = Game([Copper, Silver, Gold,
+                     Estate, Duchy, Province, Curse,
                      Chancellor, Festival, Gardens, Laboratory,
-                     Market, Smithy, Village, Woodcutter],
+                     Market, Smithy, Village, Woodcutter,
+                     Mine, Witch],
                      [strategy() for strategy in strategies])
 
         game.play()
